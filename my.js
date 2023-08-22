36207d88412b581058b31317572948121b0c372a
qwaysport <31787928+qwaysport@users.noreply.github.com>
2023-08-22 18:06:06 +0700

    Create my.js

---
+++ my.js
@@ -0,0 +1,63 @@
+jwplayer("jwplayerDiv9").setup({
+  width: "100%",
+      aspectratio: "16:9",
+      autostart: false,
+    //playlist: [{
+        file: "https://unifi-live09.secureswiftcontent.com/UnifiHD/live09.mpd",
+		//type: "dash",
+	    drm: {
+	        clearkey: {
+	          key: '65f6af8750d24b230d91b984237ac1e4',
+	          keyId: '1dfa8a645af41e83c520bddf913934ca'
+	        }
+	      }
+   // }]
+});
+
+jwplayer("jwplayerDiv3").setup({
+  width: "100%",
+      aspectratio: "16:9",
+      autostart: false,
+    //playlist: [{
+        file: "https://unifi-live05.secureswiftcontent.com/UnifiHD/live03.mpd",
+		//type: "dash",
+	    drm: {
+	        clearkey: {
+	          key: '27a2f71d87bf5eb105af096fb6605d97',
+	          keyId: '3afe30ee4ea24a67fe5a2ef06e83db0b'
+	        }
+	      }
+   // }]
+});
+
+jwplayer("jwplayerDivd").setup({
+  width: "100%",
+      aspectratio: "16:9",
+      autostart: false,
+    //playlist: [{
+        file: "https://unifi-live05.secureswiftcontent.com/UnifiHD/live06.mpd",
+		//type: "dash",
+	    drm: {
+	        clearkey: {
+	          key: 'e040ca2aa426df789ba384ba561891b4',
+	          keyId: '47a1206b44822c89320e70916cf57b0c'
+	        }
+	      }
+   // }]
+});
+
+jwplayer("jwplayerDiv8").setup({
+  width: "100%",
+      aspectratio: "16:9",
+      autostart: false,
+    //playlist: [{
+        file: "https://unifi-live05.secureswiftcontent.com/UnifiHD/live08.mpd",
+		//type: "dash",
+	    drm: {
+	        clearkey: {
+	          key: '056e1e49573d2caee5cb4fe78f8c4f3d',
+	          keyId: 'a9c619074ed38c20e25ad36c3c57c10c'
+	        }
+	      }
+   // }]
+});
